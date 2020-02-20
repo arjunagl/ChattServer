a7984495016c8f426b6d72263c88a8dc02fd910d
Arjuna Marambe <amarambe@seek.com.au>
2020-02-20 15:50:53 +1100

    dev

---
+++ api/server.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func NewServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+
+	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/greet/"):]
+		fmt.Fprintf(w, "Hello %s\n", name)
+	})
+
+	http.ListenAndServe(":9990", nil)
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/arjunagl/ChattServer/api"
+)
+
+func main() {
+	api.NewServer()
+}
