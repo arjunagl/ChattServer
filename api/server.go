f883843d4812152a836a2c161f6936f63f4795b3
Arjuna Marambe <amarambe@seek.com.au>
2020-02-22 15:41:45 +1100

    work

--- api/server.go
+++ api/server.go
@@ -2,18 +2,63 @@ package api
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func NewServer() {
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// print out that message for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
+		// send the message to the client
+		msg := []byte("Let's start to talk something.")
+		err = conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println("Successfully sent the message to the client")
+
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// helpful log statement to show connections
+	log.Println("Client Connected")
+
+	reader(ws)
+}
+
+func StartServer() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
 
-	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Path[len("/greet/"):]
-		fmt.Fprintf(w, "Hello %s\n", name)
-	})
+	http.HandleFunc("/ws", wsEndpoint)
 
 	http.ListenAndServe(":9990", nil)
 }
