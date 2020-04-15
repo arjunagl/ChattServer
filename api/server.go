3fd5998d33f8df886114a066ed383b6c21175d6c
Arjuna Marambe <amarambe@seek.com.au>
2020-04-16 07:56:08 +1000

    w

--- api/server.go
+++ api/server.go
@@ -3,7 +3,6 @@ package api
 import (
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/arjunagl/ChattServer/api/types"
 	"github.com/arjunagl/ChattServer/api/workers"
@@ -26,8 +25,11 @@ func reader(conn *websocket.Conn, clientID string) {
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Web Socket endpoint hit")
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-	clientID := strings.Split(r.URL.String(), "/")[2]
+
+	clientID := mux.Vars(r)["clientID"]
+
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
@@ -39,11 +41,11 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 // StartServer Starts the server
 func StartServer() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
 
-	r.HandleFunc("/ws/", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/ws/{clientID}", func(w http.ResponseWriter, r *http.Request) {
 		wsEndpoint(w, r)
 	})
 
@@ -51,16 +53,6 @@ func StartServer() {
 
 	}).Methods("POST")
 
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello World")
-	// })
-
-	// http.HandleFunc("/ws/", func(w http.ResponseWriter, r *http.Request) {
-	// 	wsEndpoint(w, r)
-	// })
-
-	// http.HandleFunc("/subscribe", func(w http.ResponseWriter, r *http.Request) {
-	// })
 	http.Handle("/", r)
 
 	http.ListenAndServe(":9990", nil)
